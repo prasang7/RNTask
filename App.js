0d9b94ac3c8cdbca82a5fe6e861275da224fb70f
prasang7 <prasang.sharma7@gmail.com>
2017-11-04 02:52:42 +0530

    ScrollView Functionality Added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import Header from './src/components/Header.js';
 import Banner from './src/components/section_1/Banner.js';
 import Stats from './src/components/section_2/Stats.js';
@@ -8,11 +8,15 @@ import Details from './src/components/section_3/Details.js';
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Header headerText={'BALLPRK'} />
-        <Banner />
-        <Stats />
-        <Details />
+        <ScrollView>
+          <View style={styles.container}>
+            <Banner />
+            <Stats />
+            <Details />
+          </View>
+        </ScrollView>
       </View>
     );
   }
